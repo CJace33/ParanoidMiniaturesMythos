b4c4a3716985fca2e240b488a1e5693a71a9faae
CJace33 <s176047@UCS.ac.uk>
2018-05-18 14:42:47 +0100

    Worked on updating cards more

--- MythosApp/Assets/Scripts/MasterScript.cs
+++ MythosApp/Assets/Scripts/MasterScript.cs
@@ -1,61 +1,103 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 public class MasterScript : MonoBehaviour
 {
-    List<PartyMember> partyMembers = new List<PartyMember>();
-    public GameObject cards, cardPrefab;
+    //
+    PartyMembersContainer partyMembers = new PartyMembersContainer();
+    Parties parties = new Parties();
+    public GameObject card;
+    public string basePath;
+    public string partyMembersJSONString;
+    public string partiesJSONString;
 
-	// Use this for initialization
-	void Start ()
+
+
+    // Use this for initialization
+    void Start ()
     {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-    
-    void PopulateCards()
-    {
-        foreach (PartyMember member in partyMembers)
+        //Find the path to the StreamingAssets folder based on platform
+        if (Application.isEditor)
         {
-            //For each card, instantiate a new card
-            GameObject newCard = Object.Instantiate(cardPrefab);
-            //And put it in the correct card-section
-            newCard.transform.parent = cards.transform;
-            //Then run its intialisation function with the correct values:
-            newCard.GetComponent<CardInfo>().Initialise(member);
-
+            basePath = Path.Combine(Application.dataPath, "//StreamingAssets");
+            partiesJSONString = Path.Combine(basePath, "//Parties.txt");
+            partyMembersJSONString = Path.Combine(basePath, "//PartyMembers.txt");
         }
+        //Android exception
+        else if (Application.platform == RuntimePlatform.Android)
+        {
+            basePath = "jar:file://" + Application.dataPath + "//Raw";
+            partiesJSONString = basePath + "//Parties.txt";
+            partyMembersJSONString = basePath + "//PartyMembers.txt";
+        }
+        //IOs exception
+        else if (Application.platform == RuntimePlatform.IPhonePlayer)
+        {
+            partiesJSONString = Path.Combine(Application.dataPath, "//Raw//Parties.txt");
+            partyMembersJSONString = Path.Combine(Application.dataPath, "//Raw//PartyMembers.txt");
+        }
+
+            //If the file already exists then load it, if not then create a new one at that location
+        if (File.Exists(partiesJSONString))
+            parties = LoadData(partiesJSONString, parties);
+        else
+            JSONEncode(partiesJSONString, parties);
+        //Load the data in from files
+        parties = LoadData(partiesJSONString, parties);
+        partyMembers = LoadData(partyMembersJSONString, partyMembers);
     }
-
-
-    void SerialiseFirstJson()
+    
+    //Save the data to a given path, overload for Parties or Party Members
+    public void JSONEncode(string JSONFilePath, Parties file)
     {
-        PartyMembersContainer container = new PartyMembersContainer();
-        PartyMember member = new PartyMember();
+        //Check if the Save file exists, and if it does then delete it
+        if (File.Exists(JSONFilePath))
+        {
+            File.Delete(JSONFilePath);
+        }
 
-        member.cardName = "cardnamehere";
-        member.faction = FactionEnmum.Custos_Crypta;
-        member.unflippedSprite = "unflippedSprite.png";
-        member.flippedSprite = "flippedSprite.png";
-        member.woundsCardSprite = "woundsCardSprite.png";
-
-        member.unflippedValues = {0,0,0,0,0,0,0,,0,0,0};
-        member.flippedValues;
-
-    container.members.Add(member);
-
+        //Save the level to a JSON string
+        string json = JsonUtility.ToJson(file, true);
+        //And then write it to an actual textfile
+        File.WriteAllText(JSONFilePath, json);
     }
+    public void JSONEncode(string JSONFilePath, PartyMembersContainer file)
+    {
+        //Check if the Save file exists, and if it does then delete it
+        if (File.Exists(JSONFilePath))
+        {
+            File.Delete(JSONFilePath);
+        }
+
+        //Save the level to a JSON string
+        string json = JsonUtility.ToJson(file, true);
+        //And then write it to an actual textfile
+        File.WriteAllText(JSONFilePath, json);
+    }
+
+    //Load the card data from the JSON file, the second parameter doesn't do anything except identify the load type
+    public PartyMembersContainer LoadData(string pathString, PartyMembersContainer partyMembersContainer)
+    {
+        PartyMembersContainer data = null;
+        JsonUtility.FromJsonOverwrite(pathString, data);
+        return data;
+    }
+    public Parties LoadData(string pathString, Parties parties)
+    {
+        Parties data = null;
+        JsonUtility.FromJsonOverwrite(pathString, data);
+        return data;
+    }
+
+    void ShowCard(PartyMember member)
+    {
+        card.GetComponent<CardInfo>().Initialise(member);
+    }
+
 }
-
-
-
-
-
 
 public enum FactionEnmum
 {
@@ -65,7 +107,6 @@ public enum FactionEnmum
     Priory,
     Wildborn
 }
-
 
 [System.Serializable]
 public class PartyMembersContainer
@@ -87,4 +128,21 @@ public class PartyMember
 
     //public int U_MOV1, U_MOV2, U_COM1, U_COM2, U_RAN1, U_RAN2, U_ARC1, U_ARC2, U_MYT, U_SAN;
     //public int F_MOV1, F_MOV2, F_COM1, F_COM2, F_RAN1, F_RAN2, F_ARC1, F_ARC2, F_MYT, F_SAN;
+}
+
+//Class to store a list of parties
+[System.Serializable]
+public class Parties
+{
+    public List<Party> parties;
+}
+
+//Class to store the information from each party
+[System.Serializable]
+public class Party
+{
+    public FactionEnmum faction;
+    public string partyName;
+    public List<string> partyMembers;
+
 }
